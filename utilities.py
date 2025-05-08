044e357dead6f3e4bcb11feb4407e88169e1558d
Kevin R. Geurts <kevin.r.geurts@gmail.com>
2025-05-08 15:17:01 -0500

    Added doc strings to utilities.py

--- utilities.py
+++ utilities.py
@@ -9,12 +9,23 @@ from math import atan2, pi, sqrt
 
 
 def print_strings(string_list):
+    """
+    Iterate through a list of strings and print each string.
+    """
     for string in string_list:
         print(string)
     print
 
 
 def compute_direction(x1, y1, x2, y2):
+    """
+    Compute the direction from one point to another in a 2D space.
+    :parameter x1: The x-coordinate of the first point, float or int
+    :parameter y1: The y-coordinate of the first point, float or int
+    :parameter x2: The x-coordinate of the second point, float or int
+    :parameter y2: The y-coordinate of the second point, float or int
+    :return: The direction from the first point to the second point, as a float
+    """
     if x1 == x2:
         if y1 < y2:
             direction = 7
@@ -43,12 +54,25 @@ def compute_direction(x1, y1, x2, y2):
 
 
 def distance(x1, y1, x2, y2):
+    """
+    Compute the distance between two points in a 2D space.
+    :parameter x1: The x-coordinate of the first point, float or int
+    :parameter y1: The y-coordinate of the first point, float or int
+    :parameter x2: The x-coordinate of the second point, float or int
+    :parameter y2: The y-coordinate of the second point, float or int
+    :return: The distance between the two points, as a float
+    """
     x = x2 - x1
     y = y2 - y1
     return sqrt(x * x + y * y)
 
 
 def input_double(prompt):
+    """
+    Prompt the user for a floating-point number and return it.
+    :parameter prompt: The prompt to display to the user, as a string
+    :return: The floating-point number entered by the user, or False if the input is invalid
+    """
     text = input(prompt)
     try:
         value = float(text)
